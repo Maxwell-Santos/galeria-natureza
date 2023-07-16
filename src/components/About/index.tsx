432834c00cd6e0e79a46a0a25cf31fc0cbaea1b0
Maxwell Alves Dos Santos <maxdsantos@simpress.com.br>
2023-07-15 23:24:50 -0300

    espações no sobre

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,9 +6,11 @@ export function About() {
       <p>
         Sou Luciana, uma fotógrafa apaixonada pela natureza e pelas aventuras que ela proporciona. Desde criança, fui fascinada pela vida selvagem e pelas paisagens exuberantes que o mundo natural oferece. Com minha câmera em mãos, embarco em expedições emocionantes para os lugares mais remotos e intocados do planeta.
       </p>
+      <br />
       <p>
         Cada clique é uma busca incessante pela imagem perfeita, aquela que captura a essência do momento, a energia dos animais em seus habitats naturais e a majestade das paisagens que se desdobram diante de mim. Meu coração dispara de emoção toda vez que enquadro um pássaro em pleno voo ou o pôr do sol pintando o céu com cores vibrantes.
       </p>
+        <br />
       <p>
         Minha dedicação em capturar a verdadeira beleza da natureza é uma missão pessoal. Através de minhas fotografias, desejo despertar nas pessoas o mesmo amor e respeito que sinto por esse ambiente único e delicado. Espero que, ao apreciarem minhas imagens, elas se conectem com a natureza e sintam o desejo de preservá-la para as futuras gerações. Cada click é um lembrete de que devemos proteger e valorizar nosso precioso meio ambiente.
       </p>
